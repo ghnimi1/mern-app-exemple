633aaaf09b2a6b74e9ed64e4fd37591b28b0ae5c
ghnimi1 <ghnimih@gmail.com>
2023-08-30 15:36:11 +0200

    mern app

--- client/src/App.js
+++ client/src/App.js
@@ -5,6 +5,7 @@ import HomePage from "./pages/HomePage";
 import RegisterPage from "./pages/RegisterPage";
 import LoginPage from "./pages/LoginPage";
 import { Container } from "@mui/material";
+import ProtectedRoute from "./ProtectedRoute";
 
 export default function App() {
   return (
@@ -12,7 +13,10 @@ export default function App() {
       <Header />
       <Container maxWidth="xl">
         <Routes>
-          <Route path="/" element={<HomePage />} />
+          <Route
+            path="/"
+            element={<ProtectedRoute component={<HomePage />} />}
+          />
           <Route path="/register" element={<RegisterPage />} />
           <Route path="/login" element={<LoginPage />} />
         </Routes>
